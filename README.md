03a8a9b7857b156a80fe845664bd1e9bbadc4f85
Your Name <you@example.com>
2020-09-03 13:47:08 +0100

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+## Setup 
+
+cd data 
+chmod +x getAll.sh
+./getAll.sh
+
+cd ..
+
+python bench.py
---
+++ data/getAll.sh
@@ -0,0 +1,3 @@
+wget https://reddit-meta-production.s3.amazonaws.com/distribution/publish/fortnitebr/round_3_finalized.csv
+wget https://reddit-meta-production.s3.amazonaws.com/distribution/publish/fortnitebr/round_2_finalized.csv
+wget https://reddit-meta-production.s3.amazonaws.com/distribution/publish/fortnitebr/round_1_finalized.csv
